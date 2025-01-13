5da83ff5702721788f263f4de892a6944b1dce42
andrew-brown <andrewmbrown97@gmail.com>
2025-01-13 12:11:35 -0500

    defining routes in init file

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,12 @@ def create_app(test_config=None):
   app.url_map.strict_slashes = False
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
-  ) # python functions are set by two spaces not by semicolons or
-  #curly brackets 
-  
+  )
+
+  @app.route('/hello')
+  def hello():
+    return 'Send It!'
 
   return app
+  #should use a app key when creating on server side sessions 
+   # python functions are set by two spaces not by semicolons or curley brackets
